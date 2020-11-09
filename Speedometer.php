6b7ff9f1eb437e4d843f0d1f6ca9b7927ecd382a
Yohan Tahir <70063466+YohanTr@users.noreply.github.com>
2020-11-09 14:25:35 +0100

    Update Speedometer.php

--- Speedometer.php
+++ Speedometer.php
@@ -1,16 +1,16 @@
 <?php
 
 class Speedometer {
-    const valuesMilesToKm = 1.60934;
+    const VALUESMILESTOKM = 1.60934;
 
 
     public static function convertKmToMiles(int $distance)
     {
-        return round($distance / self::valuesMilesToKm, 2);
+        return round($distance / self::VALUESMILESTOKM, 2);
 
     }
     public static function convertMilesToKm(int $distance)
     {
-        return round(self::valuesMilesToKm * $distance, 2);
+        return round(self::VALUESMILESTOKM * $distance, 2);
     }
 }
